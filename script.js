faef2bd277975fdac7f74c7ab8842b2d26c934ef
Vasantharaj Dulsan <dulsan002@gmail.com>
2025-04-19 22:37:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function rolldice(){
+const times = document.getElementById("times").value;
+const dice  = document.getElementById("dice");
+const diceimg = document.getElementById("diceimage");
+
+const value = [];
+const img =[];
+
+for(let i =0; i< times;i++){
+let result = Math.floor(Math.random()*6 + 1);
+value.push(result);
+
+img.push(`<img src="Dice_img/${result}.png" alt = "Dice${result}">`);
+}
+dice.textContent = `Dice: ${value.join(', ')}`;
+diceimg.innerHTML = img.join('');
+}
